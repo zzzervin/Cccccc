461e76304120692daa4f269793f35922c2a8280d
Ervin Liiv <77278208+zzzervin@users.noreply.github.com>
2022-03-09 09:14:47 +0200

    Update Inimene.cs

--- Inimene.cs
+++ Inimene.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,10 +9,11 @@ namespace Class_c_sharp
         string eesnimi;//поля
         int vanus;
         string staatus;
+        string perekonanimi;
         Emakeel emakeel;
         public Inimene() { }//конструктор
 
-        public Inimene(string Eesnimi,int Vanus, Emakeel emakeel)
+        public Inimene(string Eesnimi, int Vanus, Emakeel emakeel)
         {
             eesnimi = Eesnimi;
             this.emakeel = emakeel;
@@ -23,6 +24,13 @@ namespace Class_c_sharp
         {
             vanus = Vanus;
         }
+
+        public Inimene(string eesnimi, string perekonanimi, int vanus)
+        {
+            this.eesnimi = eesnimi;
+            this.perekonanimi = perekonanimi;
+        }
+
         public Emakeel Emakeel
         {
             get { return emakeel; }
@@ -32,16 +40,18 @@ namespace Class_c_sharp
         {
             set
             {
-                if (eesnimi == null) eesnimi = value; 
+                if (eesnimi == null) eesnimi = value;
             }
-            get { return eesnimi; } 
+            get { return eesnimi; }
 
 
         }//св-ва
+
+
         public int Vanus
         {
             set
-            { vanus = value;   
+            { vanus = value;
             }
             get { return vanus; }
 
@@ -49,39 +59,38 @@ namespace Class_c_sharp
         }
         public string Staatus
         {
-            
-            get 
+
+            get
             {
-                var staatus="";
-                 if (vanus < 7)
-                {staatus = "vaike laps";}
+                var staatus = "";
+                if (vanus < 7)
+                { staatus = "vaike laps"; }
                 else if (vanus < 17)
-                {  staatus = "kooli laps";}
+                { staatus = "kooli laps"; }
                 else if (vanus < 65)
-                {  staatus = "tööline laps";}
+                { staatus = "tööline laps"; }
                 else
-                {staatus = "senior";}
-                return staatus;  }
+                { staatus = "senior"; }
+                return staatus; }
 
         }
+
+
         public void Tervetame()
         {
             Console.WriteLine("Tere");
             Console.WriteLine("Minu nimi on {0} . Olen {1} aastat vana. Minu staatus {2} ", eesnimi, vanus, staatus);
+        }//метод
+        public string Perekonanimi
+        {
+            set
+            {
+                if (perekonanimi == null) perekonanimi = value;
+            }
+            get { return perekonanimi; }
+
+
         }
 
-
-        public int Palk
-        {
-            set { palk = value;}
-            get { return palk;}
-        }
-
-        public double Tulumaks()
-        {
-            double tulu = 0;
-            tulu = palk * 0.2;
-            return tulu;
         }
     }
-}
